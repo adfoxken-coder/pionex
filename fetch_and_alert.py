48e4cfcc0c7d1ba19ded7d66e04066c7386c5cd4
adfoxken-coder <adfoxken@gmail.com>
2026-09-07 19:54:31 +0800

    Update fetch_and_alert.py

--- fetch_and_alert.py
+++ fetch_and_alert.py
@@ -50,9 +50,11 @@ DEFAULT_CONFIG = {
         "VTIX", "BRK.BX", "UNHX", "GMEX", "CMCSAX", "PGX", "NFLXX", "XOMX",
         "AMBRX", "LLYX", "ABBVX", "VX", "CSCOX", "MCDX", "NVOX", "KRAQX",
         "PFEX", "INTCX", "HOODX", "AMZNX", "METAX", "COINX", "MSFTX",
-        "TQQQX", "DFDVX", "ASMLX",
+        "TQQQX", "DFDVX", "ASMLX", "TSMX",
         # 美股/韓股代幣(使用者回報確認為股票代幣)
         "AAOIX", "AXTIX", "CXMTX", "DRAMX", "SKHX",
+        # 私人公司/未上市股權相關代幣(使用者回報確認非加密貨幣)
+        "OPENAI",
         # 貴金屬
         "PPLTX", "XAU", "XAG", "XPT", "XPD", "PAXG", "XAUT",
     ],
